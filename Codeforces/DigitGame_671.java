7f25dc6d65a6c31697c7962f462ed3cdd5ab3d45
ARPIT CHAUHAN <51964839+Arpit-Chauhan@users.noreply.github.com>
2020-09-19 20:53:05 +0530

    Add files via upload

---
+++ Codeforces/DigitGame_671.java
@@ -0,0 +1,116 @@
+
+
+import java.io.*; 
+import java.util.*; 
+
+
+public class DigitGame_671
+{
+
+	public static void main(String args[])
+	{
+		
+		FastReader sc= new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+
+		int t,n,i,j,a;
+		
+		t=sc.nextInt();
+		
+		while(t-->0) {
+		a=sc.nextInt();
+		String b=sc.nextLine();
+		if(a==1) {
+		int temp=Integer.parseInt(String.valueOf(b.charAt(0)));
+		if(temp%2==0)
+			out.println(2);
+		else
+			out.println(1);
+		}
+		else {
+			if(a%2==0)
+				out.println(2);
+			if(a%2!=0)
+				out.println(1);		
+		}
+		}
+
+		out.close();
+	}
+	
+
+
+
+
+	/*
+		FASTREADER
+	*/
+	static class FastReader 
+    	{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+	
+		/*DEFINED BY ME
+		*/
+		int[] readArray(int n){
+			
+			int arr[]=new int[n];
+			
+			for(int i=0;i<n;i++)
+				arr[i]=nextInt();
+			
+			return arr;
+		}
+    } 
+}
