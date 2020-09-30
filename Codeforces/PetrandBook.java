5597ba137634aed9b848a0a71f1916e228dad204
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-09-30 20:31:24 +0530

    cf practice

---
+++ Codeforces/PetrandBook.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+
+public class PetrandBook {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        n = sc.nextInt();
+        int arr[] = sc.readArray(7);
+        int flag=0,sum=0;
+        while (true){
+            for (i = 0; i < 7; i++) {
+                sum+=arr[i];
+                if (sum>=n){
+                    flag=1;
+                    break;
+                }
+            }
+            if (flag==1)
+                break;
+        }
+
+        out.println(i+1);
+
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+    }
+}
