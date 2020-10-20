728b6d3e52abd586617f4800bfef960ea56adc89
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-10-20 22:21:15 +0530

    Codeforces_677(Div 3)

---
+++ Codeforces/DominantPirhana_677.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.util.*;
+
+
+public class DominantPirhana_677 {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j,k;
+        String s;
+
+        t = sc.nextInt();
+        while(t-->0) {
+            n = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            int b[]=Arrays.copyOf(arr,n);
+            sc.sort(b);
+            int max=b[n-1];
+            int flag=0;
+            for (i = 0; i < n-1; i++) {
+                if (arr[i] != arr[i + 1]) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag==1){
+                for (i = 0; i < n; i++) {
+                    int count=0;
+                    int temp=arr[i];
+                    j=i-1;k=i+1;
+                    while (j>=0 && arr[j]<arr[i]){
+                        if (arr[j]<arr[i])
+                            count++;
+                        j--;
+                    }while (k<n && arr[k]<arr[i]){
+                        if (arr[k]<arr[i])
+                            count++;
+                        k++;
+                    }
+                    if (arr[i]+count>max) {
+                        out.println(i + 1);
+                        break;
+                    }
+                }
+            }
+            else
+            out.println(-1);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
---
+++ Codeforces/YetAnotherBookshelf_677.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+
+
+public class YetAnotherBookshelf_677 {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        t = sc.nextInt();
+        while(t-->0) {
+            n = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            int res=Integer.MAX_VALUE;
+            int count=0,pos=-1;
+            for (i = 0; i < n-1; i++) {
+                if(arr[i]==1){
+                    pos=i;
+                    i++;
+                    while(i<n-1 && arr[i]!=1 ){
+                        i++;
+                    }
+                    if (arr[i]==1) {
+                        count += i - pos - 1;
+                        i--;
+                    }
+                }
+            }
+            out.println(count);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
