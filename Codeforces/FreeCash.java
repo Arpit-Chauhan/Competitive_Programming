aae6f3ac4e89b0d8ed65fdf81d72545bf1ac98a2
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-10-20 14:41:51 +0530

    cf practice

---
+++ Codeforces/FreeCash.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.util.*;
+
+
+public class FreeCash {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        n = sc.nextInt();
+        int temp=n;
+        List<String> ans=new ArrayList<>();
+        while(n-->0){
+            int a= sc.nextInt();
+            int b=sc.nextInt();
+            s= a+Integer.toString(b);
+            ans.add(s);
+        }
+        int count=1,res=1;
+        for (i = 1; i <temp ; i++) {
+            if (ans.get(i).equals(ans.get(i-1))) {
+                count++;
+                res = Math.max(res, count);
+            }
+            else count=1;
+        }
+        out.println(res);
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
