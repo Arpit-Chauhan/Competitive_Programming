2d78ca58f1f4597914ff5bf364eca315de22f94d
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-10-29 13:55:36 +0530

    cf practice

---
+++ Codeforces/DifferenceRow.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+
+public class DifferenceRow {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        n = sc.nextInt();
+        int arr[] = sc.readArray(n);
+        Arrays.sort(arr);
+        int temp=arr[n-1];
+        arr[n-1]=arr[0];
+        arr[0]=temp;
+        for (int item :arr)
+            out.print(item+" ");
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
