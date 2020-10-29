03d7675f3dd8016747943c50d0ef13687f8727b8
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-10-29 13:23:08 +0530

    cf practice

---
+++ Codeforces/TheFibonacciSegment.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+
+public class TheFibonacciSegment {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        n = sc.nextInt();
+        int arr[] = sc.readArray(n);
+        if (n==1)
+            out.println(1);
+        else {
+            int count = 2, res = 2;
+            for (i = 2; i < n; i++) {
+                if (arr[i] == (arr[i - 1] + arr[i - 2])) {
+                    count++;
+                    res = Math.max(res, count);
+                }
+                else
+                    count=2;
+            }
+            out.println(res);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
