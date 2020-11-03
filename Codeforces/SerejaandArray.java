73fa36282c2f11459d8fac834e6f39bf3d7cd699
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-11-03 14:19:42 +0530

    cf practice

---
+++ Codeforces/SerejaandArray.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+
+
+public class SerejaandArray {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        long k=0;
+        String s;
+
+        n = sc.nextInt();
+        int m= sc.nextInt();
+        long arr[]=new long[n];
+
+        for (i = 0; i < n; i++)
+            arr[i]= sc.nextLong();
+        long sum=0;
+        while(m-->0) {
+            t= sc.nextInt();
+            int q=sc.nextInt();
+
+            if (t==1)
+                k = sc.nextLong();
+
+            if (t==3)
+                out.println(arr[q-1]+sum);
+            else if (t==2)
+                sum+=q;
+            else
+                arr[q-1]=k-sum;
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
