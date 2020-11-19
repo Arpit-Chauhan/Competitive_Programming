814eb9d11594b8fbb18a88ead60d76b394b0e61f
Arpit-Chauhan <arpit.c10992@gmail.com>
2020-11-19 22:25:47 +0530

    Codeforces Edu Round 98

---
+++ Codeforces/TwoBrackets_EDU98.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.util.*;
+
+
+public class TwoBrackets_EDU98 {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t, n, i, j;
+        String s;
+
+        t = sc.nextInt();
+        while(t-->0) {
+            s=sc.nextLine();
+            int count=0,brp=0,brs=0;
+            for (i = 0; i < s.length(); i++) {
+                char p=s.charAt(i);
+                if (p=='(')
+                    brp++;
+                else if (p==')'){
+                    if (brp>0){
+                        brp--;
+                        count++;
+                    }
+                }
+                else if (p=='[')
+                    brs++;
+                else if (p==']'){
+                    if (brs>0){
+                        brs--;
+                        count++;
+                    }
+                }
+            }
+            out.println(count);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME
+         */
+        int[] readArray(int n) {
+
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+
+            return arr;
+        }
+
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+    }
+}
+
+
+
+
+
