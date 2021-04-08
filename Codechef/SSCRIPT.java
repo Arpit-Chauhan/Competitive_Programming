3a39c7be932dd917b21ccc92e7b89b8b40433009
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-04-08 22:56:02 +0530

    practice

---
+++ Codechef/SSCRIPT.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+
+public class SSCRIPT {
+
+    public static void main(String args[]) {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int k= sc.nextInt();
+            s= sc.nextLine();
+            int count=0;
+            int res=0;
+            for (i = 0; i < s.length(); i++) {
+                p=s.charAt(i);
+                if (p=='*'){
+                    count++;
+                    i++;
+                    while(i<n && s.charAt(i)=='*') {
+                        i++;
+                        count++;
+                        res=Math.max(count,res);
+                    }
+                    res=Math.max(count,res);
+                    count=0;
+                }
+            }
+            if (res<k)
+                out.println("NO");
+            else
+                out.println("YES");
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
---
+++ Codeforces/DominoEffect.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+
+public class DominoEffect {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc=1;
+        while (tc-- > 0) {
+            int n=sc.nextInt();
+            s= sc.nextLine();
+            int count=0;
+            boolean flagr=false;
+            int posr=-1;
+            i=0;
+                while(i<n){
+                    if (!flagr && s.charAt(i)=='.')
+                        count++;
+                    else if (s.charAt(i)=='R'){
+                        flagr=true;
+                        posr=i;
+                    }
+                    else if (s.charAt(i)=='L'){
+                        if (flagr && (posr-i)%2==0)
+                            count++;
+                        else if (!flagr)
+                            count = 0;
+                        flagr = false;
+                    }
+                    i++;
+                }
+            out.println(count);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
