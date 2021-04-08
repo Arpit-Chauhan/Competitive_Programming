bc234bbfe36b4239ef15a56e5ba3cc9c970f3e13
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-04-08 15:10:47 +0530

    practice

---
+++ Codeforces/TestingPantsforSadness.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.*;
+
+
+public class TestingPantsforSadness {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = 1;
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            long nextclicks[]=new long[n];
+            for (i = 0; i < n-1; i++) {
+                long temp=0;
+                for (j = i+1; j <n ; j++)
+                    temp+=arr[j]-1;
+                nextclicks[i]=temp;
+            }
+            nextclicks[n-1]=0;
+            long sum=0;
+            for (i = 0; i < n; i++)
+                sum+=(arr[i]-1)+nextclicks[i];
+            sum+=n;
+            out.println(sum);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
