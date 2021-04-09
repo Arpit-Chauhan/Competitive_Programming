dab5898de5c9beea416f0ba35ef182e04a24ebaf
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-04-09 17:29:16 +0530

    practice

---
+++ Codeforces/EncryptingMessages.java
@@ -0,0 +1,130 @@
+
+import java.io.*; 
+import java.util.*; 
+
+
+public class EncryptingMessages
+{
+
+	public static void main(String args[])throws IOException
+	{
+		
+		FastReader sc= new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+
+		int tc,i,j;
+		String s;char p;
+
+		tc=1;
+		while(tc-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int c= sc.nextInt();
+            int arr[]=sc.readArray(n);
+            int b[]= sc.readArray(m);
+            int temp;
+            for (i = 0; i < n-m+1; i++) {
+                temp=i;
+                for (j = 0; j <m ; j++){
+                    arr[temp]=(b[j]+arr[temp])%c;
+                    temp++;
+                }
+            }
+
+            for (int item:arr)
+                out.print(item+" ");
+        }
+		out.close();
+	}
+	
+
+
+
+
+
+	/*
+		FASTREADER
+	*/
+	static class FastReader 
+    	{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+	
+		/*DEFINED BY ME*/
+		
+		//READING ARRAY
+		int[] readArray(int n){
+			int arr[]=new int[n];
+			for(int i=0;i<n;i++)
+				arr[i]=nextInt();
+			return arr;
+		}
+		//COLLECTIONS SORT
+		 void sort(int arr[]){
+            ArrayList<Integer> l=new ArrayList<>();
+            for (int i: arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i]=l.get(i);
+        }
+        //EUCLID'S GCD
+        int gcd (int a , int b){
+            if(b!=0)
+                return gcd (b , a%b);
+            else
+                return a;
+        }
+    } 
+}
