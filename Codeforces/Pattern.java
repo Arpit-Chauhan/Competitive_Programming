fe7a0f905962d5a4dbc11fa259fc75a74e70d96f
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-04-13 21:37:27 +0530

    practice

---
+++ Codeforces/Pattern.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Pattern {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = 1;
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            String arr[]=new String[n];
+            for (i = 0; i < n; i++)
+                arr[i]=sc.nextLine();
+
+            StringBuilder ans=new StringBuilder();
+            boolean vis[]=new boolean[arr[0].length()];
+            ans.append(arr[0]);
+            for (i = 1; i < n; i++) {
+                for (j = 0; j < arr[i].length(); j++) {
+                    if (!vis[j]) {
+                        if ((ans.charAt(j) != '?' && arr[i].charAt(j) != '?') && (ans.charAt(j) != arr[i].charAt(j))) {
+                            ans.setCharAt(j, '?');
+                            vis[j] = true;
+                        }
+                        else if (ans.charAt(j)=='?' && arr[i].charAt(j)!='?')
+                            ans.setCharAt(j, arr[i].charAt(j));
+                    }
+                }
+            }
+            for (i = 0; i < ans.length(); i++)
+                if (!vis[i] && ans.charAt(i)=='?')
+                    ans.setCharAt(i,'a');
+
+            out.println(ans);
+        }
+        out.close();
+    }
+
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
