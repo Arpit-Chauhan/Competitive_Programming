10cffc66c6f363b2cb952882288f8eece735300c
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-04-15 23:11:08 +0530

    practice

---
+++ Codechef/VCOUPLE.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.util.*;
+
+
+public class VCOUPLE {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            long arr[] = sc.readArray(n);
+            long girl[]=sc.readArray(n);
+            sc.sort(arr);
+            sc.sort(girl);
+            long ans=Integer.MIN_VALUE;
+            j=0;
+            for (i = n-1; i >=0; i--) {
+                ans=Math.max(ans,girl[i]+arr[j]);
+                j++;
+            }
+            out.println(ans);
+        }
+        out.close();
+    }
+	
+
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        long[] readArray(int n) {
+            long arr[] = new long[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextLong();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(long arr[]) {
+            ArrayList<Long> l = new ArrayList<>();
+            for (long i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
