96aa625cfcf73456cc4629b73654a17d88338ec1
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-05-31 18:37:29 +0530

    practice

---
+++ Codeforces/LuckySum.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+
+
+public class LuckySum {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, j;
+        String s;
+        char p;
+
+        tc = 1;
+        while (tc-- > 0) {
+            long l = sc.nextLong();
+            long r= sc.nextLong();
+            TreeSet<Long> ans=generateLucky();
+            long sum=0;
+            while (l<=r) {
+                long h = ans.ceiling(l);
+                long t = Math.min(r, h);
+                sum += h*(t-l+1);
+                l = h+1;
+            }
+            out.println(sum);
+        }
+        out.close();
+    }
+	
+
+    public static TreeSet<Long> generateLucky(){
+        Queue<String> q=new LinkedList<>();
+        q.add("4");q.add("7");
+        TreeSet<Long> set=new TreeSet<>();
+        for (int i = 0; i <10000 ; i++) {
+            String curr=q.poll();
+            set.add(Long.parseLong(curr));
+            q.add(curr+"4");
+            q.add(curr+"7");
+        }
+        return set;
+    }
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
