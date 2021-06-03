d3214e761a4dd7b815c06cf4d60fa0c1ce310fbb
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-06-03 15:06:25 +0530

    practice

---
+++ Codeforces/SameParitySummands.java
@@ -0,0 +1,159 @@
+import java.io.*;
+import java.util.*;
+
+
+public class SameParitySummands {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int k =sc.nextInt();
+            if(k>n)
+            out.println("NO");
+            else if (k==1) {
+                out.println("YES");
+                out.println(n);
+            }
+            else if (n==k){
+                out.println("YES");
+                for (i = 0; i < n; i++)
+                    out.print(1+" ");
+                out.println();
+            }
+            else if (n%2==0){
+                if (k<=n/2){
+                    out.println("YES");
+                    for (i = 0; i < k-1; i++)
+                        out.print(2+" ");
+                    out.println(n-2*(k-1));
+                }
+                else{
+                    if (k%2==0){
+                        out.println("YES");
+                        for (i = 0; i < k-1; i++)
+                            out.print(1+" ");
+                        out.println(n-(k-1));
+                    }
+                    else
+                        out.println("NO");
+                }
+            }
+            else{
+                if (k%2==0)
+                    out.println("NO");
+                else{
+                    out.println("YES");
+                    for (i = 0; i < k-1; i++)
+                        out.print(1+" ");
+                    out.println(n-(k-1));
+                }
+            }
+        }
+        out.close();
+    }
+	
+
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+    }
+}
