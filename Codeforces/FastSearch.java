281f8993c50eb66d9ed7ad4733caec8ef71306dc
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-10-06 01:20:09 +0530

    practice

---
+++ Codeforces/FastSearch.java
@@ -0,0 +1,156 @@
+import java.io.*;
+import java.util.*;
+
+
+public class FastSearch {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = 1;
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            int k=sc.nextInt();
+            sc.sort(arr);
+            while(k-->0){
+                int x= sc.nextInt();
+                int y= sc.nextInt();
+                int ans=closeright(arr,y)-closeleft(arr,x)+1;
+                out.print(ans+" ");
+            }
+        }
+
+        out.close();
+    }
+
+
+    public static int closeleft(int arr[],int x){
+        int low=-1,high=arr.length;
+        while(low+1<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x)
+                high=mid;
+            else
+                low=mid;
+        }
+        return high+1;
+    }
+    public static int closeright(int arr[],int y){
+        int low=-1,high=arr.length;
+        while(low+1<high){
+            int mid=(low+high)/2;
+            if(arr[mid]<=y)
+                low=mid;
+            else
+                high=mid;
+        }
+        return low+1;
+    }
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+
+        void swap(int arr[], int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
