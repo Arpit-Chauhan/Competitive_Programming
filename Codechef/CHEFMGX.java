e4448f20f79cafdc863d64ddc2d0d7631e151ec7
Arpit-Chauhan <arpit.c10992@gmail.com>
2021-10-20 23:53:54 +0530

    Starters 16

---
+++ Codechef/CHEFMGX.java
@@ -0,0 +1,153 @@
+import java.io.*;
+import java.util.*;
+
+
+public class CHEFMGX {
+    static int prefix[] = new int[10000002];
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+        boolean ans[]=checkPrime(10000001);
+
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int count=y-x;
+            int temp=ans[x+1]?query(x+1,y)-1:query(x+1,y);
+            count-=temp;
+            out.println(count);
+        }
+        out.close();
+    }
+
+    public static boolean[] checkPrime(int n) {
+        boolean prime[]=new boolean[n+1];
+
+        Arrays.fill(prime, true);
+        prime[0]=prime[1]=false;
+
+        for(int i=2;i<=Math.sqrt(n);i++) {
+            for(int j=i*i;j<=n;j+=i) {
+                prime[j]=false;
+            }
+        }
+        prefix[0] = prefix[1] = 0;
+        for (int p = 2; p <= n; p++) {
+            prefix[p] = prefix[p - 1];
+            if (prime[p])
+                prefix[p]++;
+        }
+      return prime;
+    }
+    static int query(int L, int R)
+    {
+        return prefix[R] - prefix[L - 1];
+    }
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+
+        void swap(int arr[], int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
