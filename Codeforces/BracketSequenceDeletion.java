93f1e701907bb2e506f93321639877b3c463be63
Arpit-Chauhan <arpit.c10992@gmail.com>
2022-03-24 19:35:33 +0530

    Edu #125

---
+++ Codeforces/BracketSequenceDeletion.java
@@ -0,0 +1,205 @@
+import java.io.*;
+import java.util.*;
+
+
+public class BracketSequenceDeletion {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            s=sc.nextLine();
+            i=0;long count=0;
+            long pos=-1;
+            long ans=0;
+            while(i <= n - 2) {
+                if (s.charAt(i) == ')' && s.charAt(i + 1) == '(') {
+                    pos = i;
+                    i++;
+                    while (i<n && s.charAt(i) == '(')
+                        i++;
+                    if (i == n)
+                        break;
+                    long temp = i - pos + 1;
+                    ans -= temp;
+                    count++;
+                    i++;
+                }
+                else{
+                    count++;
+                    ans-=2;
+                    i+=2;
+                }
+            }
+            out.println(count+" "+(ans+n));
+        }
+        out.close();
+    }
+
+    public static boolean palin(String str)
+    {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static boolean bal(String expr)
+    {
+
+        Deque<Character> stack = new ArrayDeque<Character>();
+
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char x = expr.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{')
+            {
+
+                stack.push(x);
+                continue;
+            }
+
+
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+
+
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+
+        void swap(int arr[], int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
