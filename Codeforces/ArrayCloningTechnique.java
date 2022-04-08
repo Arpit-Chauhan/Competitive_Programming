999d0d509d7f188546fba87d530caec43c077466
Arpit-Chauhan <arpit.c10992@gmail.com>
2022-04-08 23:17:08 +0530

    #781(Div-2)

---
+++ Codeforces/ArrayCloningTechnique.java
@@ -0,0 +1,155 @@
+import java.io.*;
+import java.util.*;
+
+
+public class ArrayCloningTechnique {
+
+    public static void main(String args[]) throws IOException {
+
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tc, i, j;
+        String s;
+        char p;
+
+        tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            boolean flag=false;
+            sc.sort(arr);
+            if (arr[0]==arr[n-1])
+                flag=true;
+            if(flag || n==1)
+                out.println(0);
+            else{
+                Map<Integer,Long> map=new HashMap<>();
+                for (int it:arr)
+                    map.put(it,map.getOrDefault(it,(long)0)+1);
+                long ans=1;
+                long hfreq=-1;
+                for(Map.Entry<Integer,Long> entry:map.entrySet())
+                    hfreq=Math.max(entry.getValue(), hfreq);
+                long remain=n-hfreq;
+                while(remain!=0){
+                    if (hfreq>=remain) {
+                        ans+=remain;
+                        remain = 0;
+                        break;
+                    }
+                    else {
+                        remain -= hfreq;
+                        ans+=hfreq;
+                    }
+                    hfreq*=2;
+                    ans++;
+                }
+                out.println(ans);
+            }
+        }
+        out.close();
+    }
+	
+
+
+/*--------------------------------------------------------
+----------------------------------------------------------*/
+
+
+    //Pair Class
+
+    public static class Pair implements Comparable<Pair> {
+        int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.x - o.x;
+        }
+    }
+
+    /*
+		FASTREADER
+	*/
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        /*DEFINED BY ME*/
+
+        //READING ARRAY
+        int[] readArray(int n) {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextInt();
+            return arr;
+        }
+
+        //COLLECTIONS SORT
+        void sort(int arr[]) {
+            ArrayList<Integer> l = new ArrayList<>();
+            for (int i : arr)
+                l.add(i);
+            Collections.sort(l);
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = l.get(i);
+        }
+
+        //EUCLID'S GCD
+        int gcd(int a, int b) {
+            if (b != 0)
+                return gcd(b, a % b);
+            else
+                return a;
+        }
+
+        void swap(int arr[], int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
